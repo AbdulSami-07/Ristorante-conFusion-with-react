dc9ee2b7dc0372f6efeea6c46c49366890382f71
AbdulSami-07 <sami07israil@gmail.com>
2020-07-31 23:33:19 +0530

    React Router

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,14 +1,56 @@
 import React , { Component } from 'react';
-import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
-
+import { Navbar, NavbarBrand, Jumbotron, NavItem,Nav, Collapse, NavbarToggler } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            isNavOpen : false
+        };
+        this.toggleNav = this.toggleNav.bind(this);
+    }
+
+    toggleNav() {
+        this.setState({
+            isNavOpen : ! this.state.isNavOpen
+        })
+    }
+
     render() {
         return (
             <>      {/* short form of  React.Fragment is <> </> */}
-                <Navbar dark>
+                <Navbar dark expand="md">
                     <div className="container">
-                        <NavbarBrand href="/" >Ristorante Con Fusion</NavbarBrand>
+                        <NavbarBrand className="mr-auto" href="/" >
+                            <img src="assets/images/logo.png" height="30" width="31" alt="Ristorante Con Fusion" />
+                        </NavbarBrand>
+                        <NavbarToggler onClick={this.toggleNav} />
+                        <Collapse navbar isOpen={this.state.isNavOpen}>
+                        <Nav  navbar>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/home">
+                                        <span className="fa fa-home fa-lg"></span> Home
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/aboutus">
+                                        <span className="fa fa-info fa-lg"></span> Aboutus
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/menu">
+                                        <span className="fa fa-list fa-lg"></span> Menu
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/contactus">
+                                        <span className="fa fa-address-card fa-lg"></span> Contactus
+                                    </NavLink>
+                                </NavItem>
+                        </Nav>
+                        </Collapse>
                     </div>
                 </Navbar>
                 <Jumbotron>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,7 +3,9 @@ import Header from './HeaderComponent';
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
 import Footer from './FooterComponent'
+import Home from './HomeComponent';
 import { DISHES } from '../shared/dishes';
+import { Switch, Route, Redirect }  from 'react-router-dom';
 
 
 class Main extends Component {
@@ -16,18 +18,27 @@ class Main extends Component {
     }
   }
 
-    onDishSelect(dishId) {
-        this.setState({
-            selectedDish: dishId
-        });
-    }
+    // onDishSelect(dishId) {
+    //     this.setState({
+    //         selectedDish: dishId
+    //     });
+    // }
 
   render() {
+    const HomePage = () => {
+        return (
+            <Home />
+        );
+    }
     return (
       <div>
         <Header />
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)}/>
-        <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Switch>
+            <Route path="/home" component={HomePage} />
+            <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+            <Redirect to="/home" />
+        </Switch>
+        
         <Footer />
       </div>
     );
@@ -35,3 +46,6 @@ class Main extends Component {
 
 }
 export default Main;
+
+{/* <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)}/>
+        <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} /> */}
