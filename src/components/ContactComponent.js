01599acd32cd2ac369c9f2422f51f977ff99daec
AbdulSami-07 <sami07israil@gmail.com>
2020-08-01 14:15:16 +0530

    Assignment 2

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -12,6 +12,10 @@ function Contact(props) {
                         <BreadcrumbItem active>Contact Us</BreadcrumbItem>
                     </Breadcrumb>
                 </div>
+                <div className="col-12">
+                    <h3>Contact Us</h3>
+                    <hr />
+                </div>
             </div>
             <div className="row row-content">
                 <div className="col-12">
